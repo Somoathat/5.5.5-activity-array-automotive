cb62703c49ac93388a72a07b7ae61676de67da8f
Evan Graham <evangraham19@gmail.com>
2023-07-08 12:39:36 -0700

    changed font size on navbar

--- mercurySedan.js
+++ mercurySedan.js
@@ -9,7 +9,43 @@ console.log(v.make)
 //After you write the derived Car class, you should test it out.
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
+class Car{
+    constructor(maximumPasengers,passengers,
+        numberOFWheels,MaximumSpeed,fuel,schuduleService){
+            this.maximumPasengers=maximumPasengers
+            this.MaximumSpeed=MaximumSpeed
+            this.fuel=fuel
+            this.passengers=passengers
+            this.numberOFWheels=numberOFWheels
+            this.schuduleService=schuduleService
 
+    }
+    loadPassenger(num){
+        if(this.passengers){
+            this.passengers= this.passengers +3;
+            this.maximumPasengers= this.maximumPasengers
+            else= console.log("maximumPasengers")
+
+    }
+}
+start() {
+    if (this.fuel > 0) {
+        return this.started = true;
+        console.log("engine started...!!!");
+    } else {
+        return this.started = false;
+        console.log("engine cannot start...");
+    }
+    schuduleService(mileage){
+        this.schuduleService=this.schuduleService +3,000;
+    } else{console.log(timeForMaintenance);
+    
+
+
+    }
+    schuduleService(){
+        this.schuduleService=true;
+    }
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
